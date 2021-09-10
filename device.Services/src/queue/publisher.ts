8b35a5fb405292b5efd2a6e9cc84b65eea47bacc
modyrefy <modyrefay@gmail.com>
2021-09-10 16:45:23 +0400

    add files

--- device.Services/src/queue/publisher.ts
+++ device.Services/src/queue/publisher.ts
@@ -1,3 +1,4 @@
+/*
 var amqp = require('amqplib/callback_api');
 amqp.connect('amqp://localhost', function(error0, connection) {
     if (error0) {
@@ -5,3 +6,4 @@ amqp.connect('amqp://localhost', function(error0, connection) {
     }
     connection.createChannel(function(error1, channel) {});
 });
+*/
