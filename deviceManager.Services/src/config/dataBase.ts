8b8460c1e4cda56d66aba6d0c4ccfb3cdcfd9ac8
modyrefy <modyrefay@gmail.com>
2021-09-11 16:03:11 +0400

    add files

---
+++ deviceManager.Services/src/config/dataBase.ts
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+export const  InitConnection=async ()=>{
+    await  mongoose.connect(
+        process.env.MONGODB_CONNECTION_STRING,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            //useMongoClient: true
+        }
+    );
+    mongoose.Promise = global.Promise;
+};
